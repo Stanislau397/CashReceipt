f516b99229b1d1880fe5cc90df1c4ce86cf88003
Stanislau Kachan <Lancer397@gmail.com>
2023-02-17 18:08:36 +0300

    test: refactoring ProductRepositoryTest.java

--- src/test/java/ru/clevertec/cashreceipt/repository/ProductRepositoryTest.java
+++ src/test/java/ru/clevertec/cashreceipt/repository/ProductRepositoryTest.java
@@ -1,7 +1,9 @@
 package ru.clevertec.cashreceipt.repository;
 
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import ru.clevertec.cashreceipt.entity.Product;
@@ -15,37 +17,47 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 class ProductRepositoryTest {
 
     @Autowired
-    private ProductRepository productRepository;
-
-    @Test
-    void willSelectProductById() {
-        //given
-        Long productId = 1L;
-        Product product = Product.builder()
-                .productId(productId)
-                .price(BigDecimal.valueOf(2.5))
-                .name("Milk")
-                .promotional(true)
-                .build();
-        productRepository.save(product);
-        //when
-        Optional<Product> expectedProduct = productRepository.selectById(productId);
-        //then
-        assertThat(expectedProduct).isEqualTo(Optional.of(product));
-    }
-
-    @Test
-    void willNotSelectProductById() {
-        //given
-        Long productId = 2L;
-        //when
-        Optional<Product> expectedProduct = productRepository.selectById(productId);
-        //then
-        assertThat(expectedProduct).isEqualTo(Optional.empty());
-    }
+    private ProductRepository underTest;
 
     @AfterEach
     void tearDown() {
-        productRepository.deleteAll();
+        underTest.deleteAll();
+    }
+
+    @Nested
+    class SelectProductByIdTest {
+
+        @ParameterizedTest
+        @CsvSource(value = {
+                "1, 2.5, Milk, true",
+                "2, 10.3, Onion, false"
+        })
+        void checkSelectProductByIdShouldReturnProduct(Long productId, BigDecimal price, String name, Boolean promotional) {
+            //given
+            Product product = Product.builder()
+                    .productId(productId)
+                    .price(price)
+                    .name(name)
+                    .promotional(promotional)
+                    .build();
+            underTest.save(product);
+
+            //when
+            Optional<Product> expectedProduct = underTest.selectById(productId);
+
+            //then
+            assertThat(expectedProduct).isEqualTo(Optional.of(product));
+        }
+
+        @ParameterizedTest
+        @CsvSource(value = {
+                "1", "2"
+        })
+        void willNotSelectProductById(Long productId) {
+            //when
+            Optional<Product> expectedProduct = underTest.selectById(productId);
+            //then
+            assertThat(expectedProduct).isEmpty();
+        }
     }
 }
