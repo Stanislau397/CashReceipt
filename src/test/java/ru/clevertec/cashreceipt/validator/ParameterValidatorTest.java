8c2cc59df1869817e9f543aef639014d9225a29a
Stanislau Kachan <Lancer397@gmail.com>
2023-02-17 19:03:46 +0300

    test: refactored ParameterValidatorTest.java

--- src/test/java/ru/clevertec/cashreceipt/validator/ParameterValidatorTest.java
+++ src/test/java/ru/clevertec/cashreceipt/validator/ParameterValidatorTest.java
@@ -1,37 +1,30 @@
 package ru.clevertec.cashreceipt.validator;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class ParameterValidatorTest {
 
-    @Test
-    void isParameterValidWillReturnTrue() {
-        //given
-        String validParameter = "3-1";
+    @ParameterizedTest
+    @CsvSource(value = {
+            "3-1", "100-5", "1-100"
+    })
+    void checkIsParameterValidShouldReturnTrue(String parameter) {
         //when
-        boolean condition = ParameterValidator.isParameterValid(validParameter);
+        boolean condition = ParameterValidator.isParameterValid(parameter);
         //then
         assertThat(condition).isTrue();
     }
 
-    @Test
-    void isParameterValidWillReturnFalse() {
-        //given
-        String invalidParameter = "3";
+    @ParameterizedTest
+    @CsvSource(value = {
+            "3", "-5", "1:2"
+    })
+    void checkIsParameterValidShouldReturnFalse(String parameter) {
         //when
-        boolean condition = ParameterValidator.isParameterValid(invalidParameter);
-        //then
-        assertThat(condition).isFalse();
-    }
-
-    @Test
-    void isParameterValidWillReturnFalseBecauseParameterIsNull() {
-        //given
-        String nullParameter = null;
-        //when
-        boolean condition = ParameterValidator.isParameterValid(nullParameter);
+        boolean condition = ParameterValidator.isParameterValid(parameter);
         //then
         assertThat(condition).isFalse();
     }
