42f0ff15fbc634d987da4d08e7630968d6400018
Stanislau Kachan <Lancer397@gmail.com>
2023-02-20 21:16:58 +0300

    refactor: replacing csvSource with MethodSource and adding argumentsProvider in EntityIdValidatorTest.java

--- src/test/java/ru/clevertec/cashreceipt/validator/EntityIdValidatorTest.java
+++ src/test/java/ru/clevertec/cashreceipt/validator/EntityIdValidatorTest.java
@@ -1,30 +1,36 @@
 package ru.clevertec.cashreceipt.validator;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class EntityIdValidatorTest {
 
+    static Stream<String> validIdArgumentsProvider() {
+        return Stream.of("1", "55", "100");
+    }
+
+    static Stream<String> invalidIdArgumentsProvider() {
+        return Stream.of("1.25", "1d", "1-2");
+    }
+
     @ParameterizedTest
-    @CsvSource(value = {
-            "1", "55", "100"
-    })
-    void checkIsEntityIdValidShouldReturnTrue(String id) {
+    @MethodSource("validIdArgumentsProvider")
+    void checkIsEntityIdValidShouldReturnTrue(String validId) {
         //when
-        boolean condition = EntityIdValidator.isEntityIdValid(id);
+        boolean condition = EntityIdValidator.isEntityIdValid(validId);
         //then
         assertThat(condition).isTrue();
     }
 
     @ParameterizedTest
-    @CsvSource(value = {
-            "1.25", "1d", "1-2"
-    })
-    void checkIsEntityIdValidShouldReturnFalse(String id) {
+    @MethodSource("invalidIdArgumentsProvider")
+    void checkIsEntityIdValidShouldReturnFalse(String invalidId) {
         //when
-        boolean condition = EntityIdValidator.isEntityIdValid(id);
+        boolean condition = EntityIdValidator.isEntityIdValid(invalidId);
         //then
         assertThat(condition).isFalse();
     }
