d0395086ea8b85dae80ef2dd437c5431ac488b21
Stanislau Kachan <Lancer397@gmail.com>
2023-02-20 21:21:27 +0300

    refactor: replacing csvSource with methodSource and adding arguments provider in ParameterValidatorTest.java

--- src/test/java/ru/clevertec/cashreceipt/validator/ParameterValidatorTest.java
+++ src/test/java/ru/clevertec/cashreceipt/validator/ParameterValidatorTest.java
@@ -1,30 +1,36 @@
 package ru.clevertec.cashreceipt.validator;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class ParameterValidatorTest {
 
+    static Stream<String> validParameterArgumentsProvider() {
+        return Stream.of("3-1", "100-5", "1-100");
+    }
+
+    static Stream<String> invalidParameterArgumentsProvider() {
+        return Stream.of("3", "-5", "1:2");
+    }
+
     @ParameterizedTest
-    @CsvSource(value = {
-            "3-1", "100-5", "1-100"
-    })
-    void checkIsParameterValidShouldReturnTrue(String parameter) {
+    @MethodSource("validParameterArgumentsProvider")
+    void checkIsParameterValidShouldReturnTrue(String validParameter) {
         //when
-        boolean condition = ParameterValidator.isParameterValid(parameter);
+        boolean condition = ParameterValidator.isParameterValid(validParameter);
         //then
         assertThat(condition).isTrue();
     }
 
     @ParameterizedTest
-    @CsvSource(value = {
-            "3", "-5", "1:2"
-    })
-    void checkIsParameterValidShouldReturnFalse(String parameter) {
+    @MethodSource("invalidParameterArgumentsProvider")
+    void checkIsParameterValidShouldReturnFalse(String invalidParameter) {
         //when
-        boolean condition = ParameterValidator.isParameterValid(parameter);
+        boolean condition = ParameterValidator.isParameterValid(invalidParameter);
         //then
         assertThat(condition).isFalse();
     }
