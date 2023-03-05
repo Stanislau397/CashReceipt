570864e939a778783160de9ba2d321f77936a44e
Stanislau Kachan <Lancer397@gmail.com>
2023-03-05 23:41:08 +0300

    refactore: removing jpa repository in ProductRepository and adding EntityManager

---
+++ src/main/java/ru/clevertec/cashreceipt/repository/impl/ProductRepositoryImpl.java
@@ -0,0 +1,46 @@
+package ru.clevertec.cashreceipt.repository.impl;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.PersistenceContext;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import ru.clevertec.cashreceipt.entity.Product;
+import ru.clevertec.cashreceipt.repository.ProductRepository;
+
+import java.util.Optional;
+
+@Repository
+@AllArgsConstructor
+public class ProductRepositoryImpl implements ProductRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+
+    @Override
+    public Product save(Product product) {
+        return entityManager.merge(product);
+    }
+
+    @Override
+    public Product update(Product product) {
+        return entityManager.merge(product);
+    }
+
+    @Override
+    public Product delete(Product product) {
+        entityManager.remove(product);
+        return product;
+    }
+
+    @Override
+    public Optional<Product> selectById(Long productId) {
+        return entityManager
+                .createQuery("SELECT p FROM Product p WHERE p.productId = ?1", Product.class)
+                .setParameter(1, productId)
+                .getResultStream()
+                .findFirst();
+    }
+}
--- src/test/java/ru/clevertec/cashreceipt/repository/ProductRepositoryTest.java
+++ src/test/java/ru/clevertec/cashreceipt/repository/ProductRepositoryTest.java
@@ -1,14 +1,19 @@
 package ru.clevertec.cashreceipt.repository;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import ru.clevertec.cashreceipt.entity.Product;
+import ru.clevertec.cashreceipt.repository.impl.ProductRepositoryImpl;
 import ru.clevertec.cashreceipt.util.testbuilder.impl.ProductTestBuilder;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 import java.util.stream.LongStream;
 
@@ -17,12 +22,46 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 @DataJpaTest
 class ProductRepositoryTest {
 
-    @Autowired
     private ProductRepository productRepository;
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @BeforeEach
+    void setUp() {
+        productRepository = new ProductRepositoryImpl(entityManager);
+    }
 
     @AfterEach
     void tearDown() {
-        productRepository.deleteAll();
+        productRepository = null;
+    }
+
+    @Test
+    void update() {
+        Product product = ProductTestBuilder.aProduct().build();
+
+        productRepository.save(product);
+
+        Product updatedProduct = ProductTestBuilder.aProduct()
+                .withPromotional(true)
+                .withPrice(BigDecimal.valueOf(123.2))
+                .build();
+
+        Product actualProduct = productRepository.update(updatedProduct);
+
+        assertThat(actualProduct).isNotEqualTo(product);
+    }
+
+    @Test
+    void delete() {
+        Product product = ProductTestBuilder.aProduct().build();
+
+        Product savedProduct = productRepository.save(product);
+        productRepository.delete(savedProduct);
+
+        Optional<Product> actualProduct = productRepository.selectById(savedProduct.getProductId());
+
+        assertThat(actualProduct).isEmpty();
     }
 
     @Nested
